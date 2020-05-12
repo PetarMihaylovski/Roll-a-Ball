388f9e9c0cd69da73ad20549b053774bc5bf8cde
Petar Mihaylovski <459725@student.saxion.nl>
2020-05-12 22:39:08 +0200

    Successfully counting score

---
+++ Assets/Scripts/CollectableSpawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class CollectableSpawner : MonoBehaviour
+{
+    //publics
+    public Vector3 size;
+    public GameObject collectable;
+    public float repeatRate;
+
+    //privates
+    private int collectableCounter;
+  
+    //monos
+    void Start() {
+        //playerScore = GameObject.Find
+        InvokeRepeating("CreateCollectable", 0.5f, repeatRate);
+    }
+
+    //privates
+    private void CreateCollectable() {
+        //caluclate the new position
+        Vector3 position = new Vector3(0,0,0) + new Vector3(Random.Range(-size.x / 2, size.x / 2), 0.25f, Random.Range(-size.z / 2, size.z / 2));
+        GameObject newCollectable = Instantiate(collectable, position, Quaternion.identity);
+        //set the rotation of the new collectable
+       
+        Quaternion quaternion = new Quaternion();
+        quaternion.Set(15, 30, 45, 1);
+        newCollectable.transform.rotation = quaternion;
+
+        collectableCounter++;
+
+        //stop the spawning of new collectables
+        if (collectableCounter == 15) {
+            CancelInvoke();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    //publics
+    public float speed;
+    public Text scoreDisplayText;
+
+    //privates
+    private Rigidbody rb;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        rb = GetComponent<Rigidbody>();
+        UpdateScoreDisplay();
+    }
+
+    // FixedUpdate is called once per frame before physics is applied.
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        rb.AddForce(new Vector3(moveHorizontal, 0, moveVertical)*speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Collectable"))
+        {
+            Destroy(other.gameObject);
+            score++;
+            UpdateScoreDisplay();
+        }
+    }
+
+    private void UpdateScoreDisplay() {
+        scoreDisplayText.text = "Score: " + score.ToString();
+    }
+
+}
